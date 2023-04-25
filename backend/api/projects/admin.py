630486265a6df5a3f8b378a727e0a5204fa896bc
diddyjax19 <adetobi59@gmail.com>
2023-04-25 21:22:39 +0000

    made changes to the admin.py file in the projects app and task app in the api folder

--- backend/api/projects/admin.py
+++ backend/api/projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id','name','start_date','end_date']
