f990d95564aef1248c3a76e016428b780ceec7c3
diddyjax19 <adetobi59@gmail.com>
2023-04-26 17:12:51 +0000

    changes made to the setting.py

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-um1izz0@!67u_khhzru$e6j&^!#e@vn)3i_a=v9coyc$q=_+1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://taskit.herokuapp.com/"]
 
 
 # Application definition
