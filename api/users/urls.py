4305ab3b884feabc560fbf16e590cc8b2fbe62eb
diddyjax19 <adetobi59@gmail.com>
2023-04-26 14:18:11 +0000

    changes urls.py file in the user app

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register("",views.CreateUserView,basename="user")
+
 
 urlpatterns = [
-    
-    path('user/',include('api.users.urls')),
-    path('project/',include('api.projects.urls')),
-    path('task/',include("api.tasks.urls")),
-   
-   
+    path('token/',views.GenerateTokenView.as_view()),
+    path('create/',include(router.urls)),
+    path('search/<str:email>/',views.SearchAPIView.as_view()),
 ]
